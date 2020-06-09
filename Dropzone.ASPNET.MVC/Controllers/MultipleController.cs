5957f3e777ef1949421ced0254049fdab5fba658
jerry <howjerry22@gmail.com>
2020-06-09 08:38:50 +0800

    多檔案上傳

--- Dropzone.ASPNET.MVC/Controllers/MultipleController.cs
+++ Dropzone.ASPNET.MVC/Controllers/MultipleController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +9,41 @@ namespace Dropzone.ASPNET.MVC.Controllers
 {
     public class MultipleController : Controller
     {
-        // GET: Multiple
-        public ActionResult Index()
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        public ActionResult Index(string[] uuid)
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase[] file, string[] uuid)
+        {
+            try
+            {
+                string directoryPath = Server.MapPath("~/Upload");
+                if (file != null && file.Length > 0)
+                {
+                    for (int i = 0; i < file.Length; i++)
+                    {
+                        if (file[i] != null && file[i].ContentLength > 0)
+                        {
+                            var targetFile = file[i];
+                            string fileName = targetFile.FileName;
+                            string fileExtension = Path.GetExtension(fileName);
+                            string fileNewName = uuid[i] + fileExtension;
+                            string filePath = Path.Combine(directoryPath, fileNewName);
+                            file[i].SaveAs(filePath);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                return Json(new { message = "失敗" });
+            }
+
+            return Json(new { message = "上傳成功" });
         }
     }
 }
